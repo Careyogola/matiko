ba6fa46047ed6347d56d73d92474ec010b5d84dc
Carey Ogola <ogolacarey254@gmail.com>
2025-03-21 16:51:46 +0300

    matiko

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,41 @@
 function App() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="shadow border border-gray-200 w-96 rounded flex flex-col ">
+      <div className="border border-gray-200 w-fit rounded flex flex-col items-center py-2.5">
+        <h2 className="text-xl font-semibold text-gray-300 text-center mt-2.5">Welcome to Matiko!</h2>
+        <h5 className="text-center text-gray-400 text-medium">Create account</h5>
     <form className="flex flex-col gap-y-2.5 p-4">
     <label htmlFor="name">Name</label>
     <input 
         type="text" 
         id="name" 
         required
-        placeholder="Your Name"
-        className="p-2 border border-gray-300 rounded-md"
+        placeholder="Carey Ogola"
+        className="p-2 border border-gray-300 w-96 rounded"
     />
-    <label htmlFor="framework">Email</label>
+    <label htmlFor="email">Email</label>
     <input 
-        type="select" 
-        id="choose framework"
+        type="email" 
+        id="email"
         required
-        placeholder="Your email address"
-        className="p-2 border border-gray-300 rounded-md"
+        placeholder="eg.abc@gmail.com"
+        className="p-2 border border-gray-300 w-96 rounded"
       />
-      <label htmlFor="framework">Password</label>
+      <label htmlFor="password">Password</label>
     <input 
-        type="select" 
-        id="choose framework"
+        type="password" 
+        id="password"
         placeholder="123school@!"
-        className="p-2 border border-gray-300 rounded-md"
+        maxLength={12}
+        className="p-2 border border-gray-300 w-96 rounded"
       />
-      <button 
-          type="submit"
-          className="p-2 bg-inherit lg:w-44 border border-gray-300 shadow rounded"
-          >Submit</button>
+      
     </form>
+    <button 
+          type="submit"
+          className="p-2 bg-inherit lg:w-44 border border-gray-300 rounded hover:bg-gray-800 transition hover:text-gray-200"
+          >Sign up
+      </button>
     </div>
     </div>
   )
