af6e69402e840bcdbbc4951ba2f7f9d778b72186
Carey Ogola <ogolacarey254@gmail.com>
2025-03-23 19:43:30 +0300

    homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,11 @@
 
 function Homepage() {
     return (
-        <div className="items-center justify-center flex">Homepage</div>
+        <div className="w-full h-screen">
+            <nav className="px-5 py-3 flex flex-row items-center">
+                <h2 className="text-4xl font-medium">Ma<span className="text-4xl text-yellow-400">ti</span>ko!</h2>
+            </nav>
+        </div>
     )
 }
 
